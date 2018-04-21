7cfa6e5585972b6a685f1ba9bb48a9df6781816f
Corina Andariese <candariese@gmail.com>
2018-04-21 14:43:09 -0700

    Adding yourasquare submission

---
+++ firstSubmissions/yourasquare.js
@@ -0,0 +1,17 @@
+//NOTE: https://www.codewars.com/kata/youre-a-square
+
+//NOTE: Given an integral number, determine if it is a square number.
+
+//NOTE:
+var isSquare = function(n){
+  return false; // fix me
+}
+
+//NOTE: test cases
+
+Test.expect(!isSquare(-1), "Negative numbers cannot be square numbers");
+    Test.expect(!isSquare( 3));
+    Test.expect( isSquare( 4));
+    Test.expect( isSquare(25));
+    Test.expect(!isSquare(26));
+a
